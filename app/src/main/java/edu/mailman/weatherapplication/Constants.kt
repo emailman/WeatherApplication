3813af8abe95b148465b12c44da2c0ca24b868f2
Eric Mailman <emailman@dtcc.edu>
2023-06-26 16:40:22 -0400

    Remove check for old API version

--- app/src/main/java/edu/mailman/weatherapplication/Constants.kt
+++ app/src/main/java/edu/mailman/weatherapplication/Constants.kt
@@ -3,7 +3,6 @@ package edu.mailman.weatherapplication
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Build
 
 object Constants {
     const val APP_ID = "f282e6810344a266faf4e7311d63359d"
@@ -15,21 +14,16 @@ object Constants {
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as
                     ConnectivityManager
 
-        // Is build for version 23 or later
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val network = connectivityManager.activeNetwork ?: return false
-            val activeNetwork =
-                connectivityManager.getNetworkCapabilities(network) ?: return false
+        // For version 23 or later
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork =
+            connectivityManager.getNetworkCapabilities(network) ?: return false
 
-            return when {
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
-        } else {
-            val networkInfo = connectivityManager.activeNetworkInfo
-            return networkInfo != null && networkInfo.isConnectedOrConnecting
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
         }
     }
 }
